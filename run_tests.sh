1f017cc2930efd80c3b5f6e9ed92e77e4c546ae5
Shreya Subramanian <shsubramanian@g.hmc.edu>
2026-03-29 19:44:40 -0700

    test cases passing for normalized and de-normalized

--- run_tests.sh
+++ run_tests.sh
@@ -9,7 +9,7 @@ for problem in sql/*; do
     problem_id=$(basename ${problem%.sql})
     result="results/$problem_id.out"
     expected="expected/$problem_id.out"
-    psql < $problem > $result
+    psql postgresql://postgres:pass@localhost:1098/postgres < $problem > $result
     DIFF=$(diff -B $expected $result)
     if [ -z "$DIFF" ]; then
         echo pass
